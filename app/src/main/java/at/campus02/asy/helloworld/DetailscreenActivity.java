ac749e918049ec65c3be94f9f432ae1aed0a5762
Pawely Eskandar <p.eskandar@netconomy.net>
2016-05-21 15:10:13 +0200

    REST in Detailscreenactivity

--- app/src/main/java/at/campus02/asy/helloworld/DetailscreenActivity.java
+++ app/src/main/java/at/campus02/asy/helloworld/DetailscreenActivity.java
@@ -6,8 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
-public class DetailscreenActivity extends AppCompatActivity {
+import java.util.List;
+
+import at.campus02.asy.helloworld.objects.ElearningService;
+import at.campus02.asy.helloworld.objects.Question;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class DetailscreenActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,7 @@ public class DetailscreenActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,5 +37,27 @@ public class DetailscreenActivity extends AppCompatActivity {
             }
         });
     }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://campus02learningapp.azurewebsites.net/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    ElearningService service = retrofit.create(ElearningService.class);
+
+    service.list().enqueue(new Callback<List<Question>>() {
+        @Override
+        public void onResponse(Call<List<Question>> call, Response<List<Question>> response) {
+            ((TextView)findViewById(R.id.frage)).setText("Hello World " + response.body().size());
+        }
+
+        @Override
+        public void onFailure(Call<List<Question>> call, Throwable t) {
+
+        }
+    });
+
+
+
 
 }
