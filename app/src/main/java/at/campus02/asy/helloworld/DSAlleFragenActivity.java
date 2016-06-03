d3695d62a83e0a266c76fd3ef009ab0206ce5d9e
Pawely Eskandar <p.eskandar@netconomy.net>
2016-06-03 17:22:37 +0200

    Rest

--- app/src/main/java/at/campus02/asy/helloworld/DSAlleFragenActivity.java
+++ app/src/main/java/at/campus02/asy/helloworld/DSAlleFragenActivity.java
@@ -1,36 +1,28 @@
 package at.campus02.asy.helloworld;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import at.campus02.asy.helloworld.objects.ElearningService;
 import at.campus02.asy.helloworld.objects.Question;
-import at.campus02.asy.helloworld.objects.Quiz;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class DSAlleFragenActivity extends AppCompatActivity
 {
     private Retrofit retrofit;
     private ElearningService service;
-    private Quiz quiz;
     private TextView tvQuestion;
+    public String frage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +42,45 @@ public class DSAlleFragenActivity extends AppCompatActivity
             }
         });*/
 
+        //Retrofit
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://campus02learningapp.azurewebsites.net/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        this.service = retrofit.create(ElearningService.class);
+
+        this.service.list().enqueue(new Callback<List<Question>>() {
+            @Override
+            public void onResponse(Call<List<Question>> call, Response<List<Question>> response) {
+                // TODO
+
+
+
+                List<Question> list = response.body();
+                String[] objects =  new String[list.size()];
+
+
+                for(int i = 0; i< list.size(); i++){
+
+                    //objects[i] = list.get(i);
+
+                  Question question = (Question)list.get(i);
+                    Log.d("DSAlleFragenActivity",question.Antwort.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Question>> call, Throwable t) {
+
+                Log.v("DSAlleFragenActivity", "Fehler aufgetreten", t);
+
+            }
+        });
+
         tvQuestion = (TextView) findViewById(R.id.frage);
         if (tvQuestion != null) {
-            tvQuestion.setText(quiz.frage);
+            tvQuestion.setText(frage);
         }
         else{
             Log.d("DSAlleFragenActivity", "Fehler aufgetreten");
@@ -60,30 +88,6 @@ public class DSAlleFragenActivity extends AppCompatActivity
 
 
     }
-
-
-
-
-    /*Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://campus02learningapp.azurewebsites.net/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-
-    ElearningService service = retrofit.create(ElearningService.class);
-
-
-    // Example Service Call
-    service.list().enqueue(new Callback<List<Question>>() {
-    @Override
-    public void onResponse(Call<List<Question>> call, Response<List<Question>> response) {
-
-    }
-
-    @Override
-    public void onFailure(Call<List<Question>> call, Throwable t) {
-
-    }
-    });*/
 
 
 
